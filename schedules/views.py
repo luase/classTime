930403612a6f7ed3532fc18cce7f140895b8d31e
Jaime Alonso <js.alonsosanchez@ugto.mx>
2018-11-24 23:42:06 -0600

    Rehice el proyecto porque no servian los modelos

--- schedules/views.py
+++ schedules/views.py
@@ -1,16 +1,3 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
+from django.shortcuts import render
 
-from .models import Career
-
-class IndexView(generic.ListView):
-    template_name = 'schedules/index.html'
-    context_object_name = 'careers_list'
-    def get_queryset(self):
-        return Career.objects.all()
-"""
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-"""
+# Create your views here.
