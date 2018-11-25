ac548714bafcab01ec7eea65ed108fbcc548a266
Jaime Alonso <js.alonsosanchez@ugto.mx>
2018-11-24 23:57:26 -0600

    Cree el sitio admin

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Career, Subject, Professor, Schedule, Career_Subject, Professor_Subject_Schedule
 
 # Register your models here.
+admin.site.register(Career)
+admin.site.register(Subject)
+admin.site.register(Professor)
+admin.site.register(Schedule)
+admin.site.register(Career_Subject)
+admin.site.register(Professor_Subject_Schedule)
