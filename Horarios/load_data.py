a4c0c853fc7122e51f790b3d74f0b4a03e0b6490
Marco Diaz <marko_rkm@hotmail.com>
2018-11-29 14:41:31 -0600

    Relacion de base de datos 'Career_Subject' arreglade

--- Horarios/load_data.py
+++ Horarios/load_data.py
@@ -60,13 +60,13 @@ materias = Subject.objects.all()
 for index in materias:
     for i in range(0, len(tmp_sistemas)):
         if str(index) == str(tmp_sistemas.iloc[i]['Name']):
-            a = [Career_Subject(career=carreras[1], subject=index)]
+            a = [Career_Subject(career=carreras[0], subject=index)]
             Career_Subject.objects.bulk_create(a)
 
 for index in materias:
     for i in range(0, len(tmp_artes)):
         if str(index) == str(tmp_artes.iloc[i]['Name']):
-            a = [Career_Subject(career=carreras[0], subject=index)]
+            a = [Career_Subject(career=carreras[1], subject=index)]
             Career_Subject.objects.bulk_create(a)
 
 #Professor_Subject_Schedule Sistemas
