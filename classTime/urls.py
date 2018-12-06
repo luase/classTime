18e49558700c5df3249e616d01be77d15c93b8ec
Marco Diaz <marko_rkm@hotmail.com>
2018-12-05 21:37:26 -0600

    segunda configuracion de modificado

--- classTime/urls.py
+++ classTime/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('schedules/', include('schedules.urls')),
+    path('frontend/', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
